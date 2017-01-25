079afb9b2d81e5d5cdcbe8c5490184329f9fc33f
raghav <rarora9@sapient.com>
2017-01-25 14:45:35 +0530

    Sheduling services for Heroku

--- src/main/java/org/jarvis4ops/schedulers/ScheduledTasks.java
+++ src/main/java/org/jarvis4ops/schedulers/ScheduledTasks.java
@@ -4,7 +4,12 @@
 package org.jarvis4ops.schedulers;
 
 import org.jarvis4ops.configurations.Configurations;
+import org.jarvis4ops.controller.JiraController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -16,20 +21,56 @@ import org.springframework.web.client.RestTemplate;
  */
 @Component
 public class ScheduledTasks {
+	private static final Logger log = LoggerFactory.getLogger(JiraController.class);
 
     @Autowired
 	private Configurations configObj;
+    
+    @Autowired
+	private Environment environment;
 
-    //@Scheduled(fixedRate = 43200000)
+    @Scheduled(fixedRate = 86400000)
 	public void getPrevDayRockstarsJiraSch() {
 
 		RestTemplate restTemplate = new RestTemplate();
         MultiValueMap<String, String> headerMap = new LinkedMultiValueMap<String, String>(1);
         headerMap.add("Content-Type", "application/json");
-        String apiUrl = configObj.getHost()+configObj.getPort()+"/getPrevDayJiraRockstars";
+        String apiUrl = configObj.getHost()+environment.getProperty("server.port")+"/getPrevDayJiraRockstars";
         String response = restTemplate.getForObject(apiUrl, String.class);
-        //log.info("Response: ", response);
-        System.out.println("Response: " + response);
+        log.info("Response: ", response);
+    }
+
+    @Scheduled(fixedRate = 7200000)
+	public void checkOpenScIssuesAndPost() {
+
+		RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, String> headerMap = new LinkedMultiValueMap<String, String>(1);
+        headerMap.add("Content-Type", "application/json");
+        String apiUrl = configObj.getHost()+environment.getProperty("server.port")+"/checkOpenScIssuesAndPost";
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        log.info("Response: ", response);
+    }
+
+    @Scheduled(fixedRate = 7200000)
+	public void getOpenIncidents() {
+
+		RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, String> headerMap = new LinkedMultiValueMap<String, String>(1);
+        headerMap.add("Content-Type", "application/json");
+        String apiUrl = configObj.getHost()+environment.getProperty("server.port")+"/openIncidents";
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        log.info("Response: ", response);
+    }
+
+    @Scheduled(fixedRate = 86400000)
+	public void getDorDodJiraForShopC() {
+
+		RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, String> headerMap = new LinkedMultiValueMap<String, String>(1);
+        headerMap.add("Content-Type", "application/json");
+        String apiUrl = configObj.getHost()+environment.getProperty("server.port")+"/getDorDodJira";
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        log.info("Response: ", response);
     }
 
 }
