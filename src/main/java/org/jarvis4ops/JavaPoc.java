2050468968aff53294c7adbfb83008c1282f8a56
raghav <rarora9@sapient.com>
2017-03-03 10:43:32 +0530

    Added a new feature to give bonusly points for resolving more than X incidents in a day by folks

--- src/main/java/org/jarvis4ops/JavaPoc.java
+++ src/main/java/org/jarvis4ops/JavaPoc.java
@@ -6,18 +6,5 @@ import java.util.GregorianCalendar;
 
 public class JavaPoc {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Calendar calendar = new GregorianCalendar();
-		calendar.add(Calendar.DAY_OF_MONTH, -14);
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-	    calendar.set(Calendar.MINUTE, 0);
-	    calendar.set(Calendar.SECOND, 0);
-	    calendar.set(Calendar.MILLISECOND, 0);
-		Date forteenDaysAgo = calendar.getTime();
-		System.out.println("Date comes out to be: " + forteenDaysAgo);
-		long epoch = forteenDaysAgo.getTime();
-		System.out.println("Date comes out to be: " + epoch);
-	}
 
 }
